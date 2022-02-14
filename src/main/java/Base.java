e7762028e3225740a1dca78d0c15b3dcf9f0b11f
Vladislav-layout <62327449+Vladislav-layout@users.noreply.github.com>
2022-02-14 17:14:48 +0700

    update sum for weightBox and priceBox

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -5,17 +5,21 @@ import box.Lollipop;
 public class Base {
 
     public static void main(String[] args) {
-        double weightBox;
-        double priceBox;
+        double weightBox = 0;
+        double priceBox = 0;
         Candy candy1 = new Candy("Raffaello", 50.0, 14, "coconut");
         Candy candy2 = new Candy("Bird's milk", 35.5, 17.3, "chocolate");
         Lollipop lollipop1 = new Lollipop("chicken", 15.5, 20.4, "childhood");
-        Box[] box = {candy1, candy2, lollipop1,};
-        weightBox = candy1.getWeight() + candy2.getWeight() + lollipop1.getWeight();
-        priceBox = candy1.getPrice() + candy2.getPrice() + lollipop1.getPrice();
+        Box[] box = {candy1, candy2, lollipop1};
+
+        for (Box value : box) {
+            weightBox += value.getWeight();
+            priceBox += value.getPrice();
+        }
+
         System.out.println("Box weight = " + weightBox + ", Box price = " + priceBox);
         for (Box someBox : box) {
-            System.out.println("Sweets in a box:\n" + someBox.toString());
+            System.out.println("Sweets in a box:" + "\n" + someBox.toString());
         }
     }
 }
