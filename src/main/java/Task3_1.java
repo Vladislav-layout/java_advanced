77e3c5c8d39e028b470cc621b73433950ee47c45
Vladislav-layout <62327449+Vladislav-layout@users.noreply.github.com>
2022-02-14 16:42:03 +0700

    up. 3.1

--- src/main/java/Task3_1.java
+++ src/main/java/Task3_1.java
@@ -8,18 +8,26 @@ public class Task3_1 {
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * (20 + 1) - 10);
         }
+        System.out.println("Массив до: " + Arrays.toString(array));
         int max_negative = -10;
         int min_positive = 10;
+        int maxInd = 0;
+        int minInd = 0;
 
-        for (int j : array) {
-            if (j < 0 && j > max_negative) {
-                max_negative = j;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] < 0 && array[j] > max_negative) {
+                max_negative = array[j];
+                minInd = j;
             }
-            if (j > 0 && j < min_positive) {
-                min_positive = j;
+            if (array[j] > 0 && array[j] < min_positive) {
+                min_positive = array[j];
+                maxInd = j;
             }
         }
-        System.out.println("Массив: " + Arrays.toString(array));
+        int temp = array[minInd];
+        array[minInd] = array[maxInd];
+        array[maxInd] = temp;
+        System.out.println("Массив после: " + Arrays.toString(array));
         System.out.println("максимальный отрицательный: " + max_negative + "\nминимальный положительный: " + min_positive);
     }
 }
